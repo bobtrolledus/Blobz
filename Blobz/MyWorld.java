ace1ab689bff7adff6747dfd0ce1480b48f25c4c
panurgicpeter <10peter01.chen@gmail.com>
2023-01-10 11:37:26 -0500

    fghdgh

--- Blobz/MyWorld.java
+++ Blobz/MyWorld.java
@@ -16,21 +16,21 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(1300, 900, 1); 
+        super(1200, 800, 1); 
         setPaintOrder(Belts.class, ghostBlock.class);
         Color lightGray = new Color(228, 228, 226);
         Color gray = new Color(171, 171, 171);
         getBackground().setColor(lightGray);
         getBackground().fill();
         getBackground().setColor(gray);        
-        for(int x = 0; x <= 900; x++){
-            if(x % 45 == 0){
-                    getBackground().drawLine(x + 200, 0, x + 200, 1100);
+        for(int x = 0; x <= 800; x++){
+            if(x % 40 == 0){
+                    getBackground().drawLine(x + 200, 0, x + 200, 1000);
             }
         }
-        for(int x = 0; x < 1100; x++){
-            if(x % 45 == 0){
-                    getBackground().drawLine(200, x, 1100, x);
+        for(int x = 0; x < 1000; x++){
+            if(x % 40 == 0){
+                    getBackground().drawLine(200, x, 1000, x);
             }
         }
         prepare();
